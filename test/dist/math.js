053c6ea5085fbbabdaab20529f7c59e07e4fe1a7
hzxiaosheng <liuxiaosheng.boy@hotmail.com>
2013-12-19 16:47:35 +0800

    ignore test/dist directory

--- test/dist/math.js
+++
@@ -1,8 +0,0 @@
-define("math.js", [], function() {
-    return {
-        plus: function(a, b) {
-            return a + b;
-        }
-    };
-});
-//# buildBy cmdUtil-UglifyJS2-FullCache at Thu Dec 19 2013 16:28:35 GMT+0800 (中国标准时间) take time: 0 ms
